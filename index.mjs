690aa261621ea0103bb38174c395cfcf29ca7251
Bohdan Horbeshko <bodqhrohro@gmail.com>
2022-04-05 17:03:30 +0300

    Add threading

--- index.mjs
+++ index.mjs
@@ -1,12 +1,14 @@
+import cluster from 'cluster';
 import express from 'express';
 import fetch from 'node-fetch';
+
+const NUM_THREADS = 8;
 
 const app = express()
 
 app.get('/api/locations.json', function(req, res) {
 	fetch('https://war.ukrzen.in.ua/alerts/locations.json').then(body => body.text()).then(text => res.send(text));
 });
-
 app.get('/api/active.mp', function(req, res) {
 	fetch('https://war-api.ukrzen.in.ua/alerts/api/v3/alerts/active.mp').then(body => {
 		const data = [];
@@ -22,6 +24,7 @@ app.get('/api/active.mp', function(req, res) {
 		body.body.read();
 	});
 });
+
 app.get('/', function(req, res) {
 	res.sendFile('index.html', { root: '.' });
 });
@@ -31,4 +34,11 @@ app.get('/favicon.ico', function(req, res) {
 
 app.use('/img', express.static('img'));
 app.use('/dl', express.static('dl'));
-app.listen(80);
+
+if (cluster.isMaster) {
+	for (let i = 0; i < NUM_THREADS; i++) {
+		cluster.fork();
+	}
+} else {
+	app.listen(80);
+}
