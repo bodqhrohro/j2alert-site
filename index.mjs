84a140457dfcde6d58e48b2bd8afdfce5688abbc
Bohdan Horbeshko <bodqhrohro@gmail.com>
2022-04-12 17:28:31 +0300

    Change the upstream domain

--- index.mjs
+++ index.mjs
@@ -7,10 +7,10 @@ const NUM_THREADS = 8;
 const app = express()
 
 app.get('/api/locations.json', function(req, res) {
-	fetch('https://war.ukrzen.in.ua/alerts/locations.json').then(body => body.text()).then(text => res.send(text));
+	fetch('https://alerts.in.ua/locations.json').then(body => body.text()).then(text => res.send(text));
 });
 app.get('/api/active.mp', function(req, res) {
-	fetch('https://war-api.ukrzen.in.ua/alerts/api/v3/alerts/active.mp').then(body => {
+	fetch('https://api.alerts.in.ua/v3/alerts/active.mp').then(body => {
 		const data = [];
 
 		body.body.on('data', (chunk) => {
